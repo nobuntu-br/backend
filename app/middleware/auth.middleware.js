f98f8b1597bb6fc20247bff4c59a7101bea61495
JotaveX <jvpiloni@gmail.com>
2024-04-26 13:33:10 -0300

    Fix: Auth middleware

--- app/middleware/auth.middleware.js
+++ app/middleware/auth.middleware.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-const config = require("../config/auth.config.js");
 const db = require("../../models");
 const axios = require("axios");
 const jwkToPem = require("jwk-to-pem");
@@ -14,29 +13,67 @@ async function getJWKS(jwksUri) {
   }
 }
 
-async function verifyToken(req, res, next) {
-  let token = req.headers["x-access-token"];
+/**
+ * Verifica se é permitido acesso a rota na qual foi solicitada
+ * @param {*} req Dados da requisição
+ * @param {*} res Resposta
+ * @param {*} next Controlador do middleware
+ * @returns 
+ */
+async function verifyAccess(req, res, next) {
 
-  if (!token) {
-    return res.status(403).send({ message: "No token provided!" });
+  //Verifica se é publica, se for só passa
+  if (await isPublicRoute(req.method, req.originalUrl) == true) {
+    next();
+    return;
   }
 
-  const jwksUri =
-    "https://allystore.b2clogin.com/allystore.onmicrosoft.com/b2c_1_susi1/discovery/keys";
-  const jwk = await getJWKS(jwksUri);
-  console.log(jwk);
+  //Obtem o access_token do header
+  const access_token = req.headers["authorization"];
+
+  //Verifica se o token é valido, assim retorna o OID do usuário
+  const userOID = await verifyAccessTokenIsValid(access_token, res);
+
+  //Se não retornou o OID (não tem access_token)
+  if (userOID == null) {
+    res.status(401).send({ message: "Acesso não autorizado" });
+  } else {
+    //Se tem o OID verifica se tem permissao pra rota
+    if (await isAuthorizedOnUrl(userOID, req.method, req.originalUrl) == true) {
+      next();
+    } else {
+      console.log("não é autorizado")
+      res.status(401).send({ message: "Acesso não autorizado" });
+    }
+  }
+
+  return;
+}
+
+/**
+ * Verifica se o token de acesso é valido
+ * @param {*} access_token 
+ * @returns Retorna null (caso seja um visitante ou pessoa com token inválido) ou o OID do usuário
+ */
+async function verifyAccessTokenIsValid(access_token, res) {
+  if (!access_token) {
+    return null;
+  }
+
+  const JWKsUri = process.env.JWKsUri;
+
+  const jwk = await getJWKS(JWKsUri);
+
   if (!jwk || !jwk.length) {
-    throw new Error("Chaves JWKS não encontradas ou vazias");
+    // throw new Error("Chaves JWKS não encontradas ou vazias");
+    return null;
   }
 
   let pem = jwkToPem(jwk[0]);
-  console.log(pem);
 
   try {
-    // Verifica o token
     // ai passo o token e a key no formato pem aqui para decodificar o token e ai ele é decodificado
-    let decoded = jwt.verify(token, pem);
-    // console.log(decoded);
+    let decoded = jwt.verify(access_token, pem);
 
     const now = Math.floor(Date.now() / 1000);
     // Verifica se o token não expirou
@@ -49,43 +86,30 @@ async function verifyToken(req, res, next) {
       return res.status(403).send({ message: "Token não valido!" });
     }
 
-    console.log("Token válido.");
-  
-    req.userId = decoded.oid;
-    req.access_token = token;
-    req.exp_token = decoded.exp;
-    next();
+    return decoded.oid;
+
   } catch (error) {
     console.error("Erro ao validar o token:", error.message);
-    return false;
+    return null;
   }
 }
 
-isAuthorized = async (req, res, next) => {
-  const oid = req.userId;
+/**
+ * Verifica se é o usuário está autorizado a realizar a operação a url especificada
+ * @param {*} userOID Identificador do usuário
+ * @param {*} _method Método. @example "GET", "POST"
+ * @param {*} _url Url. @example "/produto"
+ * @returns 
+ */
+async function isAuthorizedOnUrl(userOID, _method, _url) {
 
-  try {
-    //É pego da API o usuário com base no OID
-    const _user = await db.users.findOne({ UID: oid }).exec();
-
-    if (_user != null && _user.isAdministrator != null) {
-      //Se o usuário é administrador
-      if (_user.isAdministrator == true) {
-        console.log(
-          "Usuário aceito como administrador: ",
-          _user.isAdministrator
-        );
-        next(); //Ele terá acesso a tudo
-        return;
-      }
-    }
-  } catch (error) {
-    console.error("Erro ao obter o usuário. " + error);
+  if (await userIsAdmin(userOID) == true) {
+    return true;
   }
 
   const user = await db.users.aggregate([
     // Filtrar pelo UID do usuário
-    { $match: { UID: oid } },
+    { $match: { UID: userOID } },
     // Fazer o lookup para obter os detalhes das roles
     {
       $lookup: {
@@ -128,7 +152,7 @@ isAuthorized = async (req, res, next) => {
 
     {
       $match: {
-        "FunctionSystem.route": { $eq: req.method + "#" + req.originalUrl },
+        "FunctionSystem.route": { $eq: _method + "#" + _url },
       },
     },
 
@@ -143,18 +167,93 @@ isAuthorized = async (req, res, next) => {
   ]);
 
   if (user != null && user.length > 0) {
-    next();
-    return;
+    return true;
   }
 
-  res.status(401).send({ message: "Acesso não autorizado" });
+  return false;
 };
+
+async function userIsAdmin(userOID) {
+  try {
+    //É pego da API o usuário com base no OID
+    const _user = await db.users.findOne({ UID: userOID }).exec();
+
+    if (_user != null && _user.isAdministrator != null) {
+      //Se o usuário é administrador
+      if (_user.isAdministrator == true) {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error("Erro ao obter o usuário. " + error);
+    return false;
+  }
+}
+
+async function isPublicRoute(_method, _url) {
+
+  const role = await db.roles.aggregate([
+    { $match: { name: "guest" } },
+
+    {
+      $lookup: {
+        from: "functionssystemroles",
+        localField: "FunctionSystemRoles",
+        foreignField: "_id",
+        as: "FunctionSystemRoles",
+      },
+    },
+
+    { $unwind: "$FunctionSystemRoles" },
+
+    {
+      $match: {
+        "FunctionSystemRoles.authorized": true,
+      },
+    },
+    {
+      $lookup: {
+        from: "functionssystems",
+        localField: "FunctionSystemRoles.FunctionSystem",
+        foreignField: "_id",
+        as: "FunctionSystem",
+      },
+    },
+
+    { $unwind: "$FunctionSystem" },
+
+    {
+      $match: {
+        "FunctionSystem.route": { $eq: _method + "#" + _url },
+      },
+    },
+
+    {
+      $project: {
+        name: 1,
+        NomeDaRole: "$Roles.name",
+        isAuthorized: "$FunctionSystemRoles.authorized",
+        FunctionSystemRoute: "$FunctionSystem.route",
+      },
+    },
+  ]);
+
+  if (role != null && role.length > 0) {
+    return true;
+  }
+
+  return false;
+}
 
 
 //Declaração das funções que serão exportadas
-const authJwt = {
-  verifyToken,
-  isAuthorized
+const auth = {
+  isPublicRoute,
+  isAuthorizedOnUrl,
+  verifyAccess,
+  verifyAccessTokenIsValid
 };
 
-module.exports = authJwt;
+module.exports = auth;
