64018faa407764842a6c75f179e98412c861436b
Joao Victor Piloni <jvpiloni@gmail.com>
2024-04-29 17:42:11 -0300

    Fix: campo sem updateAt

--- app/middleware/checkIfDateIsOlder.middleware.js
+++ app/middleware/checkIfDateIsOlder.middleware.js
@@ -1,40 +1,58 @@
 /**
  * Verifica se o item que está sendo alterado é de uma versão mais recente ou está sendo alterado algo que já foi alterado.
- * @param {*} mongooseModel
+ * Quando se é alterado algum registro no banco, é preciso que seja enviado pelo corpo da requisição o campo "updatedAt". 
+ * Essa variável informa a data da ultima atualização do registro que foi alterado no backEnd 
+ * @param {*} mongooseModel Modelo da Classe no mongoDb
  * @returns 
  */
 const checkIfDateIsOlder = (mongooseModel) => async (req, res, next) => {
 
-    if (!req.body.updatedAt) {
+    //Verifica se tem corpo e id do objeto que será alterado
+    if (!req.body || !req.body.id) {
+      res.status(400).json({ message: 'Nenhum valor foi recebido' });
+      return;
+    }
+  
+    try {
+      const documentId = req.body.id;
+      //Procura o objeto que será alterado no banco de dados
+      const storedDocument = await mongooseModel.findById(documentId);
+  
+      //Verifica se encontra o item que vai ser alterado no banco de dados
+      if (!storedDocument) {
+        return res.status(404).json({ message: 'Documento não encontrado' });
+      }
+  
+      //Se item não contem data de atualização, permita a alteração
+      if (storedDocument.updatedAt == null) {
+        next();
+        return;
+      }
+  
+      const storedDateUpdateDate = new Date(storedDocument.updatedAt);
+  
+      if (!storedDateUpdateDate) {
+        next();
+        return;
+      }
+  
+      //Se não obtiver dados da data de atualização do item que está sendo alterado pelo usuário, não permitir alteração
+      if (!req.body.updatedAt) {
         res.status(400).json({ message: 'Valor recebido não contém informação de quando foi alterado' });
         return;
-    }
-
-    const updatedItemUpdateDate = new Date(req.body.updatedAt);
-
-    try {
-        const documentId = req.body.id;
-        const storedDocument = await mongooseModel.findById(documentId);
-
-        if (!storedDocument) {
-            return res.status(404).json({ message: 'Documento não encontrado' });
-        }
-
-        const storedDateUpdateDate = new Date(storedDocument.updatedAt);
-
-        if (!storedDateUpdateDate) {
-            next();
-            return;
-        }
-
-        if (updatedItemUpdateDate.toISOString() === storedDateUpdateDate.toISOString()) {
-            next();
-        } else {
-            res.status(400).json({ message: 'Valor recebido é mais antigo que o valor atual armazenado' });
-        }
+      }
+  
+      const updatedItemUpdateDate = new Date(req.body.updatedAt);
+  
+      if (updatedItemUpdateDate.toISOString() === storedDateUpdateDate.toISOString()) {
+        next();
+      } else {
+        res.status(401).json({ message: 'Não é permitido realizar operação. Valor recebido é mais antigo que o valor atual armazenado' });
+      }
+  
     } catch (error) {
-        res.status(500).json({ message: 'Internal Server Error: ' });
+      res.status(500).json({ message: 'Internal Server Error: ' + error });
     }
-}
-
-module.exports = checkIfDateIsOlder;
+  }
+  
+  module.exports = checkIfDateIsOlder;
