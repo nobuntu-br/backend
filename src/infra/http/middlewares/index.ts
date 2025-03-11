7435cf118fe8d512bdf4e1db94844287bcf18df7
JoaoVictorNobuntu <117865500+JoaoVictorNobuntu@users.noreply.github.com>
2025-03-11 17:58:50 -0300

    Feat: Limit payload 50mb

--- src/infra/http/middlewares/index.ts
+++ src/infra/http/middlewares/index.ts
@@ -12,6 +12,10 @@ export function setMiddlewaresBeforeRoutes(app: Application){
     credentials: true, // Permite envio de cookies
   };
   
+  //Limita o payload da requisição
+  app.use(express.json({limit: '50mb'}));
+  app.use(express.urlencoded({limit: '50mb', extended: true }));
+  // Habilita o CORS
   app.use(cors(corsOptions));
   // parse requests of content-type - application/json
   app.use(express.json());
